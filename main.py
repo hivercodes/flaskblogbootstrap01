9fc109c6b9981c9b0c863b70573d93f85b2bd6bb
Lars <lars.eriksson.mail@gmail.com>
2022-04-26 19:04:07 +0200

    Got the form working.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -23,6 +23,19 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/form_entry", methods=["POST", "GET"])
+def receive_data():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template("form_entry.html")
+    else:
+        return render_template("contact.html")
 
 @app.route("/post/<title>")
 def post(title):
